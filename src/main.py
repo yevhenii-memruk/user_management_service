20557145b19f65b2c46d3d18500db10f1b5ea9bc
yevhenii-memruk <yevhenii.memruk@gmail.com>
2025-02-03 11:42:23 +0100

    add linters: black and isort (pyproject.toml)

--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
-from fastapi import FastAPI
 import uvicorn
+from fastapi import FastAPI
+
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-   return {"message": "Hello, world!"}
+    return {"message": "Hello, world!"}
 
 
 if __name__ == "__main__":
-   uvicorn.run("src.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)

2ef85dabe69fda0b7117f892413f5ed1698692d5
yevhenii-memruk <yevhenii.memruk@gmail.com>
2025-02-06 12:59:24 +0100

    feat: Configure logging and Pydantic settings for environment variables
    - .enf file modified, - new libraries: pydantic-setting, pre-commit, httpx

--- src/main.py
+++ src/main.py
@@ -1,13 +1,15 @@
 import uvicorn
 from fastapi import FastAPI
 
-app = FastAPI()
+from src.config import settings
+
+app = FastAPI(title=settings.PROJECT_NAME)
 
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello, world!"}
+@app.get("/healthcheck")
+async def healthcheck():
+    return {"status": "ok"}
 
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("src.main:app", host="0.0.0.0", port=8000, reload=True)

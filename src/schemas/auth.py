48b0996e899990dd169c28ac0584c40bd2969dbf
yevhenii-memruk <yevhenii.memruk@gmail.com>
2025-02-26 02:29:46 +0100

    Changes after second review

--- src/schemas/auth.py
+++ src/schemas/auth.py
@@ -4,7 +4,6 @@ from pydantic import BaseModel, Field
 class Token(BaseModel):
     access_token: str
     refresh_token: str
-    token_type: str
 
 
 class TokenRefreshRequest(BaseModel):
--- src/utils/jwt_manager.py
+++ src/utils/jwt_manager.py
@@ -1,16 +1,30 @@
+import secrets
+from dataclasses import dataclass
 from datetime import datetime, timedelta, timezone
-from typing import Any, Dict, Union
+from typing import Any
 
 import jwt
-from fastapi import HTTPException, status
+from fastapi import FastAPI, HTTPException, status
 
 from src.settings import settings
 
 
+@dataclass
+class Tokens:
+    access_token: str
+    refresh_token: str
+
+
 class JWTManager:
-    @staticmethod
+    def __init__(self, app: FastAPI):
+        self.key = settings.JWT_SECRET_KEY
+        self.algorithm = settings.JWT_ALGORITHM
+        self.expires_days = settings.JWT_EXPIRATION_DAYS
+        self.expires_minutes = settings.JWT_EXPIRATION_MINUTES
+        self.refresh_token_len = 64
+
     def _create_jwt_token(
-        data: Dict[str, Any], expires_delta: Union[timedelta, None] = None
+        self, data: dict[str, Any], expires_delta: timedelta | None = None
     ) -> str:
         """Generate a JWT token with an expired time."""
         to_encode = data.copy()
@@ -23,35 +37,31 @@ class JWTManager:
         to_encode.update({"exp": expire})
 
         return jwt.encode(
-            to_encode,
-            settings.JWT_SECRET_KEY,
-            algorithm=settings.JWT_ALGORITHM,
+            payload=to_encode,
+            key=self.key,
+            algorithm=self.algorithm,
         )
 
-    def get_tokens(self, user_id: str, role: str) -> Dict[str, str]:
+    def get_tokens(self, payload: dict) -> Tokens:
         """Generate both access and refresh tokens."""
 
         # Generate an access token (short-lived).
         access_token = self._create_jwt_token(
-            {"user_id": user_id, "role": role},
-            timedelta(minutes=settings.JWT_EXPIRE_MINUTES),
+            payload,
+            timedelta(minutes=self.expires_minutes),
         )
 
-        # Generate a refresh token (longer-lived).
-        refresh_token = self._create_jwt_token(
-            {"user_id": user_id}, timedelta(days=settings.JWT_EXPIRE_DAYS)
-        )
+        refresh_token = secrets.token_hex(self.refresh_token_len)
 
-        return {"access_token": access_token, "refresh_token": refresh_token}
+        return Tokens(access_token, refresh_token)
 
-    @staticmethod
-    def get_payload(token: str) -> Dict[str, Any]:
+    def get_payload(self, token: str) -> dict[str, Any]:
         """Verify and decode a JWT token."""
         try:
             return jwt.decode(
-                token,
-                settings.JWT_SECRET_KEY,
-                algorithms=[settings.JWT_ALGORITHM],
+                jwt=token,
+                key=self.key,
+                algorithms=[self.algorithm],
             )
         except jwt.ExpiredSignatureError:
             raise HTTPException(

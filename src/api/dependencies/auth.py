069547ac39793d010b18edb0e00b4b3c72c20f10
yevhenii-memruk <yevhenii.memruk@gmail.com>
2025-03-07 01:59:57 +0100

    Add JWTBearer class, return a valid payload

--- src/api/dependencies/auth.py
+++ src/api/dependencies/auth.py
@@ -1,45 +1,69 @@
-from fastapi import Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends, HTTPException, Request, status
+from fastapi.security import (
+    HTTPAuthorizationCredentials,
+    HTTPBearer,
+    OAuth2PasswordBearer,
+)
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.api.dependencies.database import get_session
 from src.db.models.user import Role, User
-from src.schemas.auth import TokenData
 from src.services.user import UserService
 from src.utils.jwt_manager import JWTManager
 
-oauth2_bearer = OAuth2PasswordBearer(tokenUrl="auth/token")
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl="auth/login")
+
+
+class JWTBearer(HTTPBearer):
+    def __init__(self, auto_error: bool = True):
+        super().__init__(auto_error=auto_error)
+
+    async def __call__(
+        self, request: Request, jwt_manager: JWTManager = Depends(JWTManager)
+    ) -> dict:
+        credentials: HTTPAuthorizationCredentials = await super().__call__(
+            request
+        )
+        if not credentials:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="Invalid authorization token",
+            )
+
+        try:
+            payload = jwt_manager.decode_jwt_token(credentials.credentials)
+        except HTTPException:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Could not validate credentials",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+        return payload
 
 
 async def get_current_user(
-    token: str = Depends(oauth2_bearer),
+    payload: dict = Depends(JWTBearer()),
     db: AsyncSession = Depends(get_session),
-    jwt_manager: JWTManager = Depends(JWTManager),
 ) -> User:
     """
-    Dependency to get the current user from the JWT token.
+    Dependency to get the current user from the JWT token payload.
     """
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-
-    try:
-        payload = jwt_manager.decode_jwt_token(token)
-        username = payload.get("sub")
-        if username is None:
-            raise credentials_exception
-        token_data = TokenData(username=username)
-
-    except HTTPException:
-        raise credentials_exception
+    username = payload.get("sub")
+    user_id = payload.get("id")
+    if not username or not user_id:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid token data",
+        )
 
     user_service = UserService(db)
-    user = await user_service.get_user_by_username(token_data.username)
+    user = await user_service.get_user_by_username(username)
 
-    if user is None:
-        raise credentials_exception
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="User not found",
+        )
 
     return user
 
